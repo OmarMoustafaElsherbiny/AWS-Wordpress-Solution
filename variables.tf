01dc58e0ac956a7f06a8795871e2e93c44306f25
Omar Mostafa <omar.mostafa.email@gmail.com>
2024-03-26 20:01:47 +0200

    feat: add VPC in terraform config

---
+++ variables.tf
@@ -0,0 +1,17 @@
+variable "tfc_organization" {
+  type        = string
+  description = "Value for TFC organization name"
+  sensitive   = true
+}
+
+variable "tfc_workspace_name" {
+  type        = string
+  description = "Value for TFC workspace name"
+  sensitive   = true
+}
+
+variable "aws_region" {
+  default     = "us-east-1"
+  type        = string
+  description = "Value for AWS region"
+}
