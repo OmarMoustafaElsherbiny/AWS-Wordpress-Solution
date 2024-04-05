bfcefe7be231ae0d297018dd05ed9c8afe4886bf
Omar Mostafa <omar.mostafa.email@gmail.com>
2024-04-05 16:12:54 +0200

    feat: update rds instance

--- rds.tf
+++ rds.tf
@@ -1,11 +1,30 @@
+resource "aws_db_subnet_group" "db_azs" {
+  # Name of the DB subnet group
+  name       = "main"
+  # Subnets the DB can be provisioned in
+  # TODO: Test it in the public subnet and later in the private subnet
+  subnet_ids = [aws_subnet.public.id]
+}
+
 resource "aws_db_instance" "default" {
-  allocated_storage    = 10
-  db_name              = "mydb"
+  db_name              = "wp-db"
   engine               = "mysql"
-  engine_version       = "5.7"
-  instance_class       = "db.t3.micro"
-  username             = "foo"
-  password             = "foobarbaz"
-  parameter_group_name = "default.mysql5.7"
+
+  # Recommended version req for wordpress instead of the v5.7 in the ansible playbook
+  engine_version       = "8.0.23"
+
+  # Deploy RDS in a single AZ to save costs
+  multi_az = false
+
+  # Must be unique in the AWS region namespace
+  identifier = "database-24"
+  username             = "exampleuser"
+  password             = "examplepass"
+  instance_class       = "db.t2.micro"
+  allocated_storage    = 200
+  db_subnet_group_name = aws_db_subnet_group.db_azs.name
+  vpc_security_group_ids = [aws_db_subnet_group.db_azs.id]
+  # The AZ the instance will be provisioned in
+  availability_zone = "us-east-1a"
   skip_final_snapshot  = true
 }
