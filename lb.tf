d0d9b56952f10f54bb1181347dfaea19d038bba2
Omar Mostafa <omar.mostafa.email@gmail.com>
2024-04-06 21:08:55 +0200

    feat: add lb tg attachment [skip ci]

--- lb.tf
+++ lb.tf
@@ -11,3 +11,30 @@ resource "aws_lb_target_group_attachment" "web_servers" {
   port             = 80
 }
 
+
+resource "aws_lb_listener" "my_alb_listener" {
+  load_balancer_arn = aws_lb.wp_alb.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.web_servers.arn
+  }
+}
+
+
+resource "aws_lb" "wp_alb" {
+  name               = "wp-alb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.bastion_host.id]
+  subnets            = [aws_subnet.public.id]
+
+  tags = {
+    Environment = "test"
+    ManagedBy   = "Terraform"
+    Project     = "AWS Wordpress Solution"
+    VPC         = aws_vpc.main.id
+  }
+}
