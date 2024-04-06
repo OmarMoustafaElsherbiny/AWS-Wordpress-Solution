7ba9476fd71504c4c30d6a2b9da0c63f3832f185
Omar Mostafa <omar.mostafa.email@gmail.com>
2024-04-06 21:05:47 +0200

    feat: add lb tg attachment

--- lb.tf
+++ lb.tf
@@ -5,3 +5,9 @@ resource "aws_lb_target_group" "web_servers" {
   vpc_id   = aws_vpc.main.id
 }
 
+resource "aws_lb_target_group_attachment" "web_servers" {
+  target_group_arn = aws_lb_target_group.web_servers.arn
+  target_id        = aws_instance.bastion_host.id
+  port             = 80
+}
+
