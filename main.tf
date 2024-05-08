9539eb774e268c9c8bd3e82d787a302328a3570d
Omar Mostafa <omar.mostafa.email@gmail.com>
2024-05-09 01:02:57 +0300

    feat: add vpc module multi subnet creation based on az & cidr list [skip ci]

--- main.tf
+++ main.tf
@@ -24,3 +24,15 @@ module "wp-alb" {
 
   depends_on = [ aws_instance.bastion_host ]
 }
+
+module "three_tier_vpc" {
+  source = "./modules/vpc"
+
+  name = "my-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["us-east-1a", "us-east-1b"]
+  private_subnets = ["10.0.101.0/24", "10.0.102.0/24",
+                  "10.0.103.0/24", "10.0.104.0/24"]
+  public_subnets = ["10.0.0.0/24", "10.0.1.0/24"]
+}
--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -1,16 +1,43 @@
-locals {
-  subnet = {
-    for i in var.azs : i => {
-      availability_zone    = i
-      cidr_block = cidrsubnet(var.cidr, 8, i)
-    }
-  }
+resource "aws_vpc" "this" {
+  
 }
 
 resource "aws_subnet" "public" {
-  for_each = local.subnet
-
-  vpc_id = ""
+  for_each = { for i in range(length(var.public_subnets)) : i => {
+    # use modulo to never go out of list range in case the az is shorter than public subnets cidr (from cryptographic algorithms)
+    availability_zone = var.azs[i % length(var.azs)]
+    cidr_block       = var.public_subnets[i]
+  }}
   
+  availability_zone = each.value.availability_zone
 
+  cidr_block = each.value.cidr_block
+
+  vpc_id = aws_vpc.this.id
+
+  # Enable public IP address
+  map_public_ip_on_launch = true
+
+  # Map of tags to assign to the resource.
+  tags = {
+  }
+}
+
+
+resource "aws_subnet" "private" {
+  for_each = { for i in range(length(var.private_subnets)) : i => {
+    # use modulo to never go out of list range in case the az is shorter than public subnets cidr (from cryptographic algorithms)
+    availability_zone = var.azs[i % length(var.azs)]
+    cidr_block       = var.private_subnets[i]
+  }}
+  
+  availability_zone = each.value.availability_zone
+
+  cidr_block = each.value.cidr_block
+
+  vpc_id = aws_vpc.this.id
+
+  # Map of tags to assign to the resource.
+  tags = {
+  }
 }
