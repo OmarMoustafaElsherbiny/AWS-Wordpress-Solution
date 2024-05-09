797e403ed319cd03d7ec8805e6d8876da8f3789f
Omar Mostafa <omar.mostafa.email@gmail.com>
2024-05-09 23:11:37 +0300

    refactor(vpc): add local variables to declutter the resource

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -1,14 +1,41 @@
+################################################################################
+# VPC
+################################################################################
 resource "aws_vpc" "this" {
-  
+  count = var.create_vpc ? 1 : 0
+
+  cidr_block = var.cidr
+
+  instance_tenancy     = var.instance_tenancy
+  enable_dns_hostnames = var.enable_dns_hostnames
+  enable_dns_support   = var.enable_dns_support
+
+  tags = merge(
+    { "Name" = var.name },
+    var.tags,
+    var.vpc_tags,
+  )
+}
+
+
+################################################################################
+# Public Subnets
+################################################################################
+
+locals {
+  public_subnets = { for i in range(length(var.public_subnets)) : i => {
+    az = var.azs[i % length(var.azs)]
+    cidr = var.public_subnets[i]
+  } }
 }
 
 resource "aws_subnet" "public" {
   for_each = { for i in range(length(var.public_subnets)) : i => {
     # use modulo to never go out of list range in case the az is shorter than public subnets cidr (from cryptographic algorithms)
     availability_zone = var.azs[i % length(var.azs)]
-    cidr_block       = var.public_subnets[i]
-  }}
-  
+    cidr_block        = var.public_subnets[i]
+  } }
+
   availability_zone = each.value.availability_zone
 
   cidr_block = each.value.cidr_block
@@ -28,9 +55,9 @@ resource "aws_subnet" "private" {
   for_each = { for i in range(length(var.private_subnets)) : i => {
     # use modulo to never go out of list range in case the az is shorter than public subnets cidr (from cryptographic algorithms)
     availability_zone = var.azs[i % length(var.azs)]
-    cidr_block       = var.private_subnets[i]
-  }}
-  
+    cidr_block        = var.private_subnets[i]
+  } }
+
   availability_zone = each.value.availability_zone
 
   cidr_block = each.value.cidr_block
