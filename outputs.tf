7886c066af17e860f5194a9c6a9405343ad475cb
Omar Mostafa <omar.mostafa.email@gmail.com>
2024-05-24 14:23:13 +0300

    refactor(root module): clean the root module outputs

--- outputs.tf
+++ outputs.tf
@@ -1,31 +1,34 @@
-output "bastion_hostname" {
-  value       = aws_instance.bastion_host.public_dns
-  description = "The public domain of the bastion host"
-}
+################################################################################
+# Endpoints 
+################################################################################
 
-output "bastion_ipv4_address" {
-  value       = aws_instance.bastion_host.public_ip
-  description = "The public IP address of the bastion host"
-}
-
+# instead of MySQL container name as the endpoint, we use RDS & in the CI workflow it edits the playbook to add the endpoint
 output "rds_endpoint" {
   value       = aws_db_instance.default.endpoint
-  description = "The RDS endpoint"
+  description = "The RDS endpoint to be used with wordpress"
 }
 
 output "alb_dns_name" {
   value       = module.wp-alb.alb_dns_name
-  description = "The DNS name of the ALB from ALB module"
+  description = "The DNS name of the ALB for the user to connect to"
 }
+
+
+################################################################################
+# Ansible variables
+################################################################################
 
 output "ec2s_insatance_ids" {
   value = module.ec2s.ec2_instance_id 
+  description = "List of EC2 instance IDs for Ansible to use with SSH & AWS CLI to connect to the instances"
 }
 
 output "ec2_instance_id_1" {
   value = module.ec2s.instance_ids[0] 
+  description = "Instance ID of the first EC2 instance used in Ansible SSH proxy command to connect to the instance"
 }
 
 output "ec2_instance_id_2" {
   value = module.ec2s.instance_ids[1] 
+  description = "Instance ID of the second EC2 instance used in Ansible SSH   proxy command to connect to the instance"
 }
